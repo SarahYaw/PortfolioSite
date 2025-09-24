5413e5704b92be86789c26444e3c62d072f48f2f
SarahYaw <sarahyaw16@gmail.com>
2025-09-23 21:57:14 -0400

    about page work/light refactoring for better reading/add todos

---
+++ website/data.py
@@ -0,0 +1,247 @@
+code_projects = [
+    {
+        'name':'Form to PDF Python Program',
+        'description':"Program that lets the user import a CSV of questions and options into this program, answer the questions, export a PDF of the filled out form. Built in python using tkinter for the UI.",
+        'skills':'Python (Programming Language) · Tkinter · Project Planning · Application Development · Requirements Gathering',
+        'src':'/static/assets/code/formToPdf.png',
+        'alt':'Plain form with example questons and answers. Black text on a gray background.',
+        'link':'https://github.com/SarahYaw/simpleFormToPDF',
+        'caption':"I've built this as a base for a project I'm building for a relative who has to fill out the same form every other week who's just editing PDFs all the time, hopefully this makes things easier. I'm going to make a private fork off of this so I can specify it to their situation. Any part that I looked up a lot for I put the link in the code right near it",
+    },
+    {
+        'name':'Medication Manager React.js Project',
+        'description':"Webpage that allows you to enter medications for it to track when you're due for a refill, how often you take it, who prescribed it, etc. Basic webpage with a basic back-end that gives you a CRUD interface for a table of entries. Made to brush up on React.js.",
+        'skills':'React.js · Node.js · Tailwind CSS · TypeScript · User Interface Design · LoopBack.io',
+        'src':'/static/assets/code/medManager.png',
+        'alt':'Chart listing med name, dosage, description, morning dose, afternoon dose, evening dose, provider, last refilled, and buttons to refill, edit, and remove the medication from the list',
+        'link':'https://github.com/SarahYaw/MedManager',
+        'caption':'Made this as a proof-of-concept for a relative that wanted a more at-a-glance way to keep track of their medications.',
+    },
+    {
+        'name':'AudioAI',
+        'description':"Web application that allows users to use LLM's and AI voice software to create novel renditions of spec spots and other segments to fill air time. Connects to quite a few different automation softwares to air the tracks on-time.",
+        'skills':'GitHub · Wireframing · Node.js · Visual Studio · Web Applications · Bootstrap (Framework) · Web Application Development · Front-End Development · Software as a Service (SaaS) · JavaScript · Application Development · Full-Stack Development · Application Programming Interfaces (API) · Cascading Style Sheets (CSS) · Git · TypeScript · Django · Project Development · REST APIs · Software Development · User Experience (UX) · Python (Programming Language) · Back-End Web Development · HTML · Vue.js · Web Development',
+        'src':'/static/assets/code/audioai.png',
+        'alt':'audio ai user interface showing the various adjustments one can make to a voice.',
+        'link':'https://futurimedia.com/solutions/audioai/',
+        'caption':'AI empowered web software that allows instantaneous production of audio commercials, spec spots, and other clips to be played over radio.',
+    },
+    {
+        'name':'Beacon',
+        'description':'This beacon mobile application is what the end-users will interact with. I did work on the administrative web application, but that is not open to the general public.',
+        'skills':'GitHub · Wireframing · Node.js · Visual Studio · Web Applications · Vuetify · Web Application Development · Front-End Development · Software as a Service (SaaS) · JavaScript · Application Development · Full-Stack Development · Application Programming Interfaces (API) · Cascading Style Sheets (CSS) · Git · Project Development · REST APIs · Software Development · User Experience (UX) · Python (Programming Language) · HTML · Vue.js · Web Development',
+        'src':'/static/assets/code/beacon.png',
+        'alt':'beacon mobile application.',
+        'link':'https://www.beaconalwayson.com/',
+        'caption':'Safety alert system that sends audio and text emergency and warning alerts via radio and downloadable mobile application by location from multiple official sources.',
+    },
+    {
+        'name':'SpotOn',
+        'description':"Web application that allows users to use LLM's and AI voice software to create novel renditions of spec spots and other segments to fill air time.",
+        'skills':'GitHub · Wireframing · Node.js · Visual Studio · Web Applications · Bootstrap (Framework) · Web Application Development · Front-End Development · Software as a Service (SaaS) · JavaScript · Application Development · Full-Stack Development · Application Programming Interfaces (API) · Cascading Style Sheets (CSS) · Git · TypeScript · Django · Project Development · REST APIs · Software Development · User Experience (UX) · Python (Programming Language) · Back-End Web Development · HTML · Programming · Web Development',
+        'src':'/static/assets/code/spoton.png',
+        'alt':'Spot On user interface showing a soundwave under the header Current Spot, and options to manage the spec spot and adjust audio levels.',
+        'link':'https://futurimedia.com/solutions/spoton/',
+        'caption':'One-Click spec spots and audio commercials.',
+    }
+]
+art_projects = [
+    {
+        'name':'art',
+        'description':'art',
+        'skills':'PythonGathering',
+        'src':'/static/assets/art/PeerReviewCrossStitch.jpg',
+        'alt':'art',
+        'link':'art',
+        'caption':'art',
+    },
+    {
+        'name':'art2',
+        'description':'art2',
+        'skills':'PythonGathering',
+        'src':'/static/assets/art/pumpkinHead.jpg',
+        'alt':'art2',
+        'link':'art2',
+        'caption':'art2',
+    },
+    {
+        'name':'art2',
+        'description':'art2',
+        'skills':'PythonGathering',
+        'src':'/static/assets/art/TissueCoverAbleSisters.jpg',
+        'alt':'art2',
+        'link':'art2',
+        'caption':'art2',
+    },
+]
+# TODO: edit descriptions such that no bullet points are necessary
+timeline_items = [
+    {
+        'title':'IT/Developer & Systems Analyst',
+        'employer':'Rock Solid Logistics, Inc. · Full-time',
+        'duration': 'Jun 2025 - Present',
+        'location': 'United States · Remote',
+        'description': '''
+            - Maintain and improve Rock Solid's website and customer-facing digital tools.
+            - Develop and manage internal dashboards to improve broker and dispatcher workflow.
+            - Troubleshooting and tech support as issues arise.- Maintain and improve Rock Solid's website and customer-facing digital tools.
+            - Develop and manage internal dashboards to improve broker and dispatcher workflow.
+            - Troubleshooting and tech support as issues arise.
+        ''',
+        'skills': 'Application Development · Application Programming Interfaces (API) · Back-End Web Development · Bootstrap (Framework) · Cascading Style Sheets (CSS) · Communication · Database Design · Front-End Development · Full-Stack Development · Git · Graphic Design · HTML · HTML5 · JavaScript · MySQL · Problem Solving · Programming · Programming Languages · Project Development · Project Planning · Requirements Gathering · SQL · Software Development · User Experience (UX) · User Interface Design · Visual Studio · Web Application Development · Web Applications · Web Development · Wireframing',
+    },
+    {
+        'link': 'https://github.com/SarahYaw/PortfolioSite',
+        'title': 'Portfolio Site',
+        'employer':'Project',
+        'duration':'Aug 2025 - Present',
+        'description': 'Just a nice little portfolio website to show off projects and summaries',
+        'skills': 'HTML · CSS · JavaScript · Python · Python Flask · Web Applications · User Interface Design · Git · Visual Studio · User Experience (UX)',
+    },
+    {
+        'link': 'https://github.com/SarahYaw/MedManager',
+        'title': 'Medication Manager',
+        'employer':'Project',
+        'duration':'Apr 2025 - May 2025',
+        'description': "Webpage that allows you to enter meds for it to track when you're due for a refill, how often you take its, who prescribed it, etc.",
+        'skills': 'React.js · Node.js · Tailwind CSS · TypeScript · User Interface Design · LoopBack.io',
+    },
+    {
+        'link': 'https://github.com/SarahYaw/simpleFormToPDF',
+        'title': 'Simple Form To PDF Program',
+        'employer':'Project',
+        'duration':'Mar 2022 - Apr 2025',
+        'description': 'import a csv of questions and answers into this program, answer the questions, export a pdf of the filled out form.',
+        'skills': 'Python (Programming Language) · Tkinter · Project Planning · Application Development · Requirements Gathering',
+    },
+    {
+        'title': 'Full-stack Developer',
+        'employer':'Futuri · Full-time',
+        'duration':'Mar 2022 - Mar 2025',
+        'location':'United States · Remote',
+        'description': 'Development and maintenance across multiple software projects. Most using a Vue.js frontend and Django python or a PHP backend.',
+        'skills': 'Front-End Development · HTML · REST APIs · Python · Back-End Web Development · Vuetify · TypeScript · Communication · Web Applications · HTML5 · Full-Stack Development · Jira · Problem Solving · Django REST Framework · Trello · Application Development · User Interface Design · JavaScript · PHP · Programming Languages · Software Development · Web Application Development · Git · Project Development · Software as a Service (SaaS) · Django · Visual Studio · Cascading Style Sheets (CSS) · Node.js · Database Design · Web Development · User Experience (UX) · Wireframing · Application Programming Interfaces (API) · Vue.js · GitHub',
+    },
+    {
+        'link': "https://futurimedia.com/solutions/audioai/",
+        'title': 'AudioAI',
+        'employer':'Project',
+        'duration':'Apr 2022 - Mar 2025',
+        'description': 'AI empowered web software that allows instantaneous production of audio commercials, spec spots, and other clips to be played over radio.',
+        'skills': 'GitHub · Wireframing · Node.js · Visual Studio · Web Applications · Communication · Bootstrap (Framework) · Web Application Development · Front-End Development · Software as a Service (SaaS) · JavaScript · Application Development · Full-Stack Development · Application Programming Interfaces (API) · Cascading Style Sheets (CSS) · Git · TypeScript · Django · Project Development · REST APIs · Programming Languages · Software Development · User Experience (UX) · Python (Programming Language) · Back-End Web Development · HTML · Vue.js · Programming · Web Development',
+    },
+    {
+        'link': 'https://futurimedia.com/solutions/spoton/',
+        'title': 'SpotOn',
+        'employer':'Project',
+        'duration':'Aug 2022 - Mar 2025',
+        'description': 'One-Click spec spots and audio commercials.',
+        'skills': 'GitHub · Wireframing · Node.js · Visual Studio · Web Applications · Communication · Bootstrap (Framework) · Web Application Development · Front-End Development · Software as a Service (SaaS) · JavaScript · Application Development · Full-Stack Development · Application Programming Interfaces (API) · Cascading Style Sheets (CSS) · Git · TypeScript · Django · Project Development · REST APIs · Programming Languages · Software Development · User Experience (UX) · Python (Programming Language) · Back-End Web Development · HTML · Programming · Web Development',
+    },
+    {
+        'link': 'https://futurimedia.com/solutions/beacon/',
+        'title': 'Beacon',
+        'employer':'Project',
+        'duration':'Mar 2024 - Mar 2025',
+        'description': 'Safety alert system that sends audio and text emergency and warning alerts via radio and downloadable mobile application by location from multiple official sources.',
+        'skills': 'GitHub · Wireframing · Node.js · Visual Studio · Web Applications · Communication · Vuetify · Web Application Development · Front-End Development · Software as a Service (SaaS) · Application Development · Full-Stack Development · Cascading Style Sheets (CSS) · Git · Project Development · REST APIs · Programming Languages · Software Development · User Experience (UX) · Python (Programming Language) · HTML · Vue.js · Programming · Web Development',
+    }, 
+    {
+        'title': 'Graduate Assistant',
+        'employer': 'Eastern Michigan University · Full-time',
+        'duration': 'Sep 2020 - Dec 2021',
+        'location': 'Ypsilanti, Michigan, United States',
+        'description':'''
+            - Graded homework assignments and projects to give feedback on learning goal progression.
+            - Assisted students during class with learning to code as well as any technical difficulties.
+            - Assisted in development of assignments that ensure a solid understanding of foundational concepts for the class.
+            - Tutoring hours outside of normal class functioning to assist other students with any questions they may have.- Graded homework assignments and projects to give feedback on learning goal progression. - Assisted students during class with learning to code as well as any technical difficulties. - Assisted in development of assignments that ensure a solid understanding of foundational concepts for the class. - Tutoring hours outside of normal class functioning to assist other students with any questions they may have.
+        ''',
+        'skills':'Communication · Computer Science · Java · Problem Solving · English · Programming Languages · Node.js'
+    }, 
+    {
+        'title': 'Web Design Intern',
+        'employer': 'Rock Solid Logistics, Inc. · Internship',
+        'duration': 'Apr 2021 - Aug 2021',
+        'location': 'Brighton, Michigan, United States',
+        'description': '''
+            - Re-building the company website using Drupal.
+            - Communicating with different employees to ensure a well-rounded design.- Re-building the company website using Drupal. - Communicating with different employees to ensure a well-rounded design.
+        ''',
+        'skills': 'Communication · Problem Solving · English · Programming Languages · Software Development · Project Development · Programming · Wireframing'
+    },
+    {
+        'link': 'https://github.com/SarahYaw/PRNG',
+        'title': 'Pseudo-Random Number Generator',
+        'employer': 'Project',
+        'duration': 'Jan 2021 - Apr 2021',
+        'description': 'A program of different PRNG algorithms researched and coded up for an Independent Study.',
+        'skills': 'Java · GitHub · Computer Science · English · Programming Languages · Software Development · Programming'
+    },
+    {
+        'title': 'Teaching Assistant',
+        'employer': 'Eastern Michigan University · Full-time',
+        'duration': 'Jan 2020 - Apr 2020',
+        'location': 'Ypsilanti, Michigan, United States',
+        'description': '''
+            - Graded homework assignments and projects to give feedback on learning goal progression..
+            - Assisted students during class with learning to code as well as any technical difficulties.
+            - Assisted in development of assignments that ensure a solid understanding of foundational concepts for the class.- Graded homework assignments and projects to give feedback on learning goal progression.. - Assisted students during class with learning to code as well as any technical difficulties. - Assisted in development of assignments that ensure a solid understanding of foundational concepts for the class.
+        ''',
+        'skills': 'Communication · Computer Science · HTML5 · Java · Problem Solving · JavaScript · Programming Languages · Cascading Style Sheets (CSS)'
+    },
+    {
+        'title': 'Crop Yield Predictor',
+        'employer': 'Project',
+        'duration': 'Jan 2020 - Apr 2020',
+        'description': 'Program that predicts crop yields for Bay County Michigan given precipitation, temperature, and yield data from previous years.',
+        'skills': 'Java · GitHub · Computer Science · Programming Languages · Software Development · ProgrammingSkills: Java · GitHub · Computer Science · Programming Languages · Software Development · Programming'
+    },
+    {
+        'link': 'https://github.com/crypt0d1v3r/Checkers',
+        'title': 'Checkers AI',
+        'employer': 'Project',
+        'duration': 'Sep 2019 - Dec 2019',
+        'description': 'A program that uses AI principals to play checkers against the user. I chiefly did the user Interface for this project.',
+        'skills': ' Java · GitHub · Computer Science · Communication · Git · Programming Languages · Software Development · Graphic Design · Programming'
+    },
+    {
+        'title': 'Graphic Designer',
+        'employer': 'The Honors College at Eastern Michigan University · Part-time',
+        'duration': 'Sep 2017 - Apr 2020',
+        'location': 'Ypsilanti, Michigan, United States',
+        'description': '''
+            - Collaborated with the programming team to create printed event flyers and social media graphics.
+            - Collaborated with the computing team to create accessible user interfaces for applications.
+            - Designed pamphlets to advertise and inform students about various events being hosted by The Honors College.
+            - Drafted scholarship application forms to ensure efficient and accessible application and data entry.
+            - Promoted The Honors College to the students at EMU in an appealing and accessible way.- Collaborated with the programming team to create event flyers and social media graphics. - Collaborated with the computing team to create accessible user interfaces for applications. - Designed pamphlets to advertise and inform students about various events being hosted by The Honors College. - Drafted scholarship application forms to ensure efficient and accessible application and data entry. - Promoted The Honors College to the students at EMU in an appealing and accessible way.
+        ''',
+        'skills': 'Communication · Social Media · English · Graphic Design · Social Media Graphic Design'
+    },
+    {
+        'title': 'Teaching Assistant',
+        'employer': 'Eastern Michigan University · Full-time',
+        'duration': 'Jun 2018 - Jun 2018',
+        'location': 'Ypsilanti, Michigan, United States',
+        'description':
+        '''
+            Teaching assistant for a week-long camp called Bits&Bytes
+            - Taught middle school students basic computer science concepts to introduce them to STEM career choices.
+            - Created lesson plans that teach introductory concepts and skills and maintain engagement at a middle school level.Teaching assistant for a week-long camp called Bits&Bytes - Taught middle school students basic computer science concepts to introduce them to STEM career choices. - Created lesson plans that teach introductory concepts and skills and maintain engagement at a middle school level.
+        ''',
+        'skills': 'HTML · Communication · Computer Science · Java · Problem Solving · English · Programming Languages'
+    },
+    {
+
+        'title': "Master's degree, Computer Science",
+        'location': 'Eastern Michigan University',
+        'duration': 'Sep 2020 - Dec 2021'
+    },
+    {
+        'title': "Bachelors of Science degree, Computer Science",
+        'employer': 'Minor in Mathematics',
+        'location': 'Eastern Michigan University',
+        'duration': 'Sep 2016 - Apr 2020'
+    }
+]
--- website/router.py
+++ website/router.py
@@ -1,6 +1,7 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
+from .data import art_projects, code_projects, timeline_items
 
 bp = Blueprint('main', __name__) 
 
@@ -11,123 +12,15 @@ def index():
 
 @bp.route('/about')
 def about():
-    return render_template('about.html')
+    context={}
+    context['timeline_items'] = timeline_items
+    return render_template('about.html', context=context)
 
 @bp.route('/projects')
 def projects():
     context={}
-    context['code_projects'] = [
-        {
-            'name':'Form to PDF Python Program',
-            'description':"Program that lets the user import a CSV of questions and options into this program, answer the questions, export a PDF of the filled out form. Built in python using tkinter for the UI.",
-            'skills':'Python (Programming Language) · Tkinter · Project Planning · Application Development · Requirements Gathering',
-            'src':'/static/assets/code/formToPdf.png',
-            'alt':'Plain form with example questons and answers. Black text on a gray background.',
-            'link':'https://github.com/SarahYaw/simpleFormToPDF',
-            'caption':"I've built this as a base for a project I'm building for a relative who has to fill out the same form every other week who's just editing PDFs all the time, hopefully this makes things easier. I'm going to make a private fork off of this so I can specify it to their situation. Any part that I looked up a lot for I put the link in the code right near it",
-        },
-        {
-            'name':'Medication Manager React.js Project',
-            'description':"Webpage that allows you to enter medications for it to track when you're due for a refill, how often you take it, who prescribed it, etc. Basic webpage with a basic back-end that gives you a CRUD interface for a table of entries. Made to brush up on React.js.",
-            'skills':'React.js · Node.js · Tailwind CSS · TypeScript · User Interface Design · LoopBack.io',
-            'src':'/static/assets/code/medManager.png',
-            'alt':'Chart listing med name, dosage, description, morning dose, afternoon dose, evening dose, provider, last refilled, and buttons to refill, edit, and remove the medication from the list',
-            'link':'https://github.com/SarahYaw/MedManager',
-            'caption':'Made this as a proof-of-concept for a relative that wanted a more at-a-glance way to keep track of their medications.',
-        },
-        {
-            'name':'AudioAI',
-            'description':"Web application that allows users to use LLM's and AI voice software to create novel renditions of spec spots and other segments to fill air time. Connects to quite a few different automation softwares to air the tracks on-time.",
-            'skills':'GitHub · Wireframing · Node.js · Visual Studio · Web Applications · Bootstrap (Framework) · Web Application Development · Front-End Development · Software as a Service (SaaS) · JavaScript · Application Development · Full-Stack Development · Application Programming Interfaces (API) · Cascading Style Sheets (CSS) · Git · TypeScript · Django · Project Development · REST APIs · Software Development · User Experience (UX) · Python (Programming Language) · Back-End Web Development · HTML · Vue.js · Web Development',
-            'src':'/static/assets/code/audioai.png',
-            'alt':'audio ai user interface showing the various adjustments one can make to a voice.',
-            'link':'https://futurimedia.com/solutions/audioai/',
-            'caption':'AI empowered web software that allows instantaneous production of audio commercials, spec spots, and other clips to be played over radio.',
-        },
-        {
-            'name':'Beacon',
-            'description':'This beacon mobile application is what the end-users will interact with. I did work on the administrative web application, but that is not open to the general public.',
-            'skills':'GitHub · Wireframing · Node.js · Visual Studio · Web Applications · Vuetify · Web Application Development · Front-End Development · Software as a Service (SaaS) · JavaScript · Application Development · Full-Stack Development · Application Programming Interfaces (API) · Cascading Style Sheets (CSS) · Git · Project Development · REST APIs · Software Development · User Experience (UX) · Python (Programming Language) · HTML · Vue.js · Web Development',
-            'src':'/static/assets/code/beacon.png',
-            'alt':'beacon mobile application.',
-            'link':'https://www.beaconalwayson.com/',
-            'caption':'Safety alert system that sends audio and text emergency and warning alerts via radio and downloadable mobile application by location from multiple official sources.',
-        },
-        {
-            'name':'SpotOn',
-            'description':"Web application that allows users to use LLM's and AI voice software to create novel renditions of spec spots and other segments to fill air time.",
-            'skills':'GitHub · Wireframing · Node.js · Visual Studio · Web Applications · Bootstrap (Framework) · Web Application Development · Front-End Development · Software as a Service (SaaS) · JavaScript · Application Development · Full-Stack Development · Application Programming Interfaces (API) · Cascading Style Sheets (CSS) · Git · TypeScript · Django · Project Development · REST APIs · Software Development · User Experience (UX) · Python (Programming Language) · Back-End Web Development · HTML · Programming · Web Development',
-            'src':'/static/assets/code/spoton.png',
-            'alt':'Spot On user interface showing a soundwave under the header Current Spot, and options to manage the spec spot and adjust audio levels.',
-            'link':'https://futurimedia.com/solutions/spoton/',
-            'caption':'One-Click spec spots and audio commercials.',
-        },
-        # {
-        #     'name':'code2',
-        #     'description':'code2',
-        #     'skills':'PythonGathering',
-        #     'src':'/static/assets/PennyMabelLeaves.jpg',
-        #     'alt':'code2',
-        #     'link':'code2',
-        #     'caption':'code2',
-        # },
-    ]
-    context['art_projects'] = [
-        {
-            'name':'art',
-            'description':'art',
-            'skills':'PythonGathering',
-            'src':'/static/assets/art/PeerReviewCrossStitch.jpg',
-            'alt':'art',
-            'link':'art',
-            'caption':'art',
-        },
-        {
-            'name':'art2',
-            'description':'art2',
-            'skills':'PythonGathering',
-            'src':'/static/assets/art/pumpkinHead.jpg',
-            'alt':'art2',
-            'link':'art2',
-            'caption':'art2',
-        },
-        {
-            'name':'art2',
-            'description':'art2',
-            'skills':'PythonGathering',
-            'src':'/static/assets/art/TissueCoverAbleSisters.jpg',
-            'alt':'art2',
-            'link':'art2',
-            'caption':'art2',
-        },
-        # {
-        #     'name':'art2',
-        #     'description':'art2',
-        #     'skills':'PythonGathering',
-        #     'src':'/static/assets/art/pumpkinHead.jpg',
-        #     'alt':'art2',
-        #     'link':'art2',
-        #     'caption':'art2',
-        # },
-        # {
-        #     'name':'art2',
-        #     'description':'art2',
-        #     'skills':'PythonGathering',
-        #     'src':'/static/assets/art/pumpkinHead.jpg',
-        #     'alt':'art2',
-        #     'link':'art2',
-        #     'caption':'art2',
-        # },
-        # {
-        #     'name':'art2',
-        #     'description':'art2',
-        #     'skills':'PythonGathering',
-        #     'src':'/static/assets/art/pumpkinHead.jpg',
-        #     'alt':'art2',
-        #     'link':'art2',
-        #     'caption':'art2',
-        # },
-    ]
+    context['code_projects'] = code_projects
+    context['art_projects'] = art_projects
     return render_template('projects.html', context=context)
 
 
